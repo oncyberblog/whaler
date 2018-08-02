0252794fccdc1dc0163971f5d8dd238fb3b53e45
oncyber <oncyber@protonmail.com>
2018-08-02 19:17:31 +0100

    working on tcpdump issues

--- reset-reploy.sh
+++ reset-reploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo rm -rf /var/tmp/whaler
-docker stop $(docker ps -aq); docker rm $(docker ps -aq); docker network prune -f; docker-compose build && docker-compose up
+docker stop $(docker ps -aq); docker rm $(docker ps -aq); docker network prune -f; docker-compose build && docker-compose up -d
