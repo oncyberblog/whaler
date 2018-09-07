fe8eccdfd92d0ace6e11c4ce2759fb88a979f497
oncyber <oncyber@protonmail.com>
2018-09-07 07:52:27 +0100

    fixed log rollover on startup

--- agent/modules/__init__.py
+++ agent/modules/__init__.py
@@ -5,13 +5,18 @@ from Configuration import Configuration
 LOG_FILE=Configuration().get("dataDirectory") + "/whaler.log"
 
 #logging - to file and console
-should_roll_over = os.path.isfile(LOG_FILE)
-handler = logging.handlers.RotatingFileHandler(LOG_FILE, mode='w', backupCount=1000)
-if should_roll_over:  # log already exists, roll over!
-    handler.doRollover()
+
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(module)s - %(message)s', filename=LOG_FILE,level=logging.DEBUG, filemode='w')
+
+#add console
 console = logging.StreamHandler()
 console.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(module)s - %(message)s'))
 console.setLevel(logging.INFO)
 logging.getLogger('').addHandler(console)
+
+#roll logs
+handler = logging.handlers.RotatingFileHandler(filename=LOG_FILE, backupCount=1000)
+logging.getLogger('').addHandler(handler)
+if os.path.isfile(LOG_FILE):
+    handler.doRollover()
