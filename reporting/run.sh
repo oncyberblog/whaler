6a528cf1b045e30e1b41c809f5d8d7e8ce4ec398
oncyber <oncyber@protonmail.com>
2018-09-29 09:33:34 +0100

    cleanup

--- reporting/run.sh
+++
@@ -1,8 +0,0 @@
-#generate a random URL to serve content from
-UID=`dd if=/dev/urandom count=4 bs=1 | md5sum | cut -d" " -f1`
-
-sed -i -e "s|UID|$UID|g" /etc/nginx/conf.d/default.conf
-
-echo "Reporting Server: serving reports on http://localhost/$UID"
-
-nginx -g "daemon off;"
